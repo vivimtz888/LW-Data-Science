de81c0fdd08da966738027caf987f94581ff46f1
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-08-03 11:05:05 -0500

    Radius and perimeter for a circle and a disk

---
+++ playground/playground.py
@@ -0,0 +1,10 @@
+# TODO: it's a playground, let's write some code (no unit tests to run here)
+from math import pi
+
+radius = 5
+perimeter = pi * 2 * radius
+area = (pi) * (radius ** 2)
+
+print(f"The radius is set to {radius}")
+print(f"Perimeter of the circle is {round(perimeter, 1)}")
+print(f"The area of the disk is {round(area,1)}")
